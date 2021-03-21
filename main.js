4023a74a7c0dcee15e168da54a3f512053f8c8b8
gerlinda137 <gerlinda137@gmail.com>
2021-03-21 22:44:45 +0300

    added styles via js

--- main.js
+++ main.js
@@ -38,3 +38,84 @@ fetch(
     // document.querySelector('.preloader').style.display = 'none';
     // document.querySelector('.weather-load-error').style.display = 'flex';
   });
+
+  function createStyles() {
+    let style = document.createElement('style');
+    style.innerHTML = `
+    .thread {
+      max-width: 300px;
+      padding-top: 20px;
+      width: 100%;
+      position: sticky;
+      right: 0;
+      bottom: 0;
+      background-color: #ffffff;
+      font-family: Arial, sans-serif;
+    }
+    .thread-list {
+      display: flex;
+      flex-direction: column;
+      style-list: none;
+      padding-left: 0;
+    }
+    .card {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-self: center;
+      margin: 0 auto;
+      margin-bottom: 10px;
+      padding: 20px;
+      border-radius: 5px;
+      box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.1);
+      color: #000000;
+      font-size: 12px;
+    }
+    .card__title {
+      font-family: $montserrat;
+      font-weight: bold;
+      font-size: 14px;
+      line-height: 16px;
+    }
+    
+    .card__author {
+      color: #8b2222;
+      margin-bottom: 5px;
+    }
+    
+    .card__date {
+      color: gray;
+      position: relative;
+      padding-left: 20px;
+    }
+    
+    .card__date::before {
+      content: '';
+      position: absolute;
+      width: 15px;
+      height: 15px;
+      background-image: url('https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/9-512.png');
+      top: 0;
+      left: 0;
+    }
+      
+    .card__data-wrapper {
+      display: flex;
+      justify-content: space-between;
+    }
+  
+    .card__link {
+      color: inherit;
+      text-decoration: underline;
+      text-decoration-color: #8b2222;
+    }
+  
+    .card__status {
+      text-align: right;
+      opacity: 0.4;
+    }
+    `;
+    document.head.appendChild(style);
+  }
+createStyles();
+  
